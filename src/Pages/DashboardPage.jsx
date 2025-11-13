e823dbbbd6a639df992afa131b586a948cf60b03
fawwazmak <fawwazmakinde47@gmail.com>
2025-11-13 23:45:48 +0100

    hhhh

--- src/Pages/DashboardPage.jsx
+++ src/Pages/DashboardPage.jsx
@@ -4,7 +4,6 @@ import Dashboard from '../components/dashboard/Dashboard';
 const DashboardPage = () => {
   return (
     <>
-      <h1 className='bg-white w-full'>lorem</h1>
       <Dashboard />
     </>
   );
